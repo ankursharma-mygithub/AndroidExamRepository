7a6b453004d7bffc1b5ad098cb6595b8ac8d4b79
Ankur Sharma <ankursharma012@gmail.com>
2018-03-11 19:07:11 +0300

    1. Updated unit test case.

--- app/src/androidTest/java/com/android/testproject/amazingcanada/MainGalleryActivityTest.java
+++ app/src/androidTest/java/com/android/testproject/amazingcanada/MainGalleryActivityTest.java
@@ -1,85 +1,103 @@
-package com.android.testproject.amazingcanada;
+package com.android.testproject.canadafacts;
 
 /**
- * Created by ankursharma on 3/7/18.
+ * Created by ankursharma on 3/8/18.
  */
 
-import android.support.test.espresso.IdlingPolicies;
-import android.support.test.espresso.IdlingRegistry;
-import android.support.test.espresso.IdlingResource;
-import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.text.format.DateUtils;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 
+import com.android.testproject.amazingcanada.R;
 import com.android.testproject.amazingcanada.ui.MainGalleryActivity;
+import com.android.testproject.amazingcanada.ui.MainGalleryAdapter;
 
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-import java.util.concurrent.TimeUnit;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static junit.framework.Assert.assertEquals;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 public class MainGalleryActivityTest {
+
+    private static final String TAG = "MainGalleryActivityTest";
+
+    private static final long WAIT_TIME = 5000;
+    private static final int COUNT_ITEMS = 13;
     @Rule
-    public ActivityTestRule<MainGalleryActivity> activityRule = new ActivityTestRule<>(MainGalleryActivity.class);
+    public ActivityTestRule<MainGalleryActivity> activityTestRule = new ActivityTestRule<>(MainGalleryActivity.class);
 
-    @Before
-    public void resetTimeout() {
-        IdlingPolicies.setMasterPolicyTimeout(DateUtils.SECOND_IN_MILLIS * 3, TimeUnit.SECONDS);
-        IdlingPolicies.setIdlingResourceTimeout(DateUtils.SECOND_IN_MILLIS * 3, TimeUnit.SECONDS);
-    }
-
-    //BELOW ARE THE TWO INSTRUMENTED TESTS AND ONE OF THIS WILL FAIL IF THE INPUT IS NOT CORRECT.
-    //Scroll to a particular position in recylerview
+    /**
+     * UI test assering screen state when all data is present.
+     * Also tests if all the items in the JSON file are listed or not.
+     * ASSUMPTION : Device is connected to internet.
+     * SUCCESS: When device is connected to internet.
+     * FAILS : When device disconnected from internet, and when the items displayed in list is different
+     *         different than actual items retrieved from JSON file.
+     */
     @Test
-    public void test1ScrollToPosition() {
-        //Assume waiting time as 3 seconds - meanwhile the UI sets up.
-        final long waitingTime = DateUtils.SECOND_IN_MILLIS * 3;
-
-        // Now we wait
-        IdlingResource idlingResource = new ElapsedIdlingResource(waitingTime);
-        IdlingRegistry.getInstance().register(idlingResource);
-
-        onView(ViewMatchers.withId(R.id.recyclerView))
-                .perform(RecyclerViewActions.scrollToPosition(8));
-
+    public void checkAllItemsFromJsonLoaded() throws InterruptedException {
+        //Put sleep here to wait for recyclerview to initialize.
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.sleep(WAIT_TIME);
+        }catch (InterruptedException e) {
+            Log.e(TAG, e.toString());
         }
+        if(isNetworkConnected(activityTestRule.getActivity())) {
+            //Get recyclerview
+            RecyclerView recyclerView = (RecyclerView) activityTestRule.getActivity().findViewById(R.id.recyclerView);
+            Espresso.onView(ViewMatchers.withId(R.id.recyclerView)).check(matches(isDisplayed()));
+            //Check if all items are loaded or not. Assume number of items in list are 13.
+            MainGalleryAdapter adapter = (MainGalleryAdapter)(recyclerView.getAdapter());
 
-
-        // Clean up
-        IdlingRegistry.getInstance().unregister(idlingResource);
+            int count = activityTestRule.getActivity().mPresenter.getItemsCount();
+            //Ensure that all elements from list are displayed in the recyclerview.
+            assertEquals(adapter.getItemCount(), count);
+        }
     }
 
-    //Test click on any of the item of recyclerview
+    /**
+     * Asserting the screen state in error state(no internet)
+     * Assumption : Device is not connected to internet.
+     * SUCCESS: When device is disconnected from internet.
+     */
     @Test
-    public void test2ClickOnListItem() {
-        //Assume waiting time as 1 seconds - meanwhile the UI sets up.
-        final long waitingTime = DateUtils.SECOND_IN_MILLIS * 1;
-        // Now we wait
-        IdlingResource idlingResource = new ElapsedIdlingResource(waitingTime);
-        IdlingRegistry.getInstance().register(idlingResource);
+    public void checkErrorMessageIsDisplayedForNoInternet()  {
+        //Put sleep here to wait for recyclerview to initialize.
+        try {
+            Thread.sleep(WAIT_TIME);
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.toString());
+        }
+        //If device is not connected to internet than the applications hould show an error dialog.
+        //Here checking if the dialog si displayed with proper message or not.
+        String errorMessage = "Please connect to internet to view interested things about Canada.";
+        if(!isNetworkConnected(activityTestRule.getActivity())) {
+            Espresso.onView(ViewMatchers.withText(errorMessage)).check(matches(isDisplayed()));
+        }
+    }
 
-        onView(ViewMatchers.withId(R.id.recyclerView))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(2,
-                        click()));
-
-        // Clean up
-       IdlingRegistry.getInstance().unregister(idlingResource);
+    /**
+     * Check if the device is connected to internet or not.
+     */
+    private boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 
 }
